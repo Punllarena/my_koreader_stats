77aabe6c527f326486cb4ab7b5e00966a0e36965
Pun <puncedy@gmail.com>
2026-01-23 10:54:44 +0800

    added books emoji

--- stat_output.py
+++ stat_output.py
@@ -50,7 +50,7 @@ for (year, month), titles in sorted(monthly_data.items()):
 
         f.write("\n")
         f.write(f"## ⏱ Total Read Time: {seconds_to_hm(total_time)}\n")
-        f.write(f"## Total Titles: {len(titles)}\n")
+        f.write(f"## 📚 Total Titles: {len(titles)}\n")
 
 conn.close()
 
