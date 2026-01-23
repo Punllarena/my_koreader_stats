4a1172678053308437ba3040e480591edef34ff4
Pun <puncedy@gmail.com>
2026-01-23 10:55:16 +0800

    Update stat_output_yearly.py

--- stat_output_yearly.py
+++ stat_output_yearly.py
@@ -48,7 +48,6 @@ for year, titles in sorted(yearly_titles.items()):
             f.write(f"- {title} : {seconds_to_hm(seconds)}\n")
 
         f.write(f"\n## 📚 Total Titles: {total_titles}\n\n")
-        f.write("---\n\n")
         f.write("## ⏱ Total Reading Time\n")
         f.write(f"{seconds_to_hm(total_time)}\n")
 
